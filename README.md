f17e8bcac70e2434414754a3c3478570b0cfc8e5
Galster2010 <segom0574@gmail.com>
2021-01-12 21:20:19 +0300

    static branch first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Among Us Game settings unlocker
+## What does it do?
+As you know, in Among Us you can't change map or impostor amount unless you recreate a lobby. We don't know why Innersloth did like that, but we don't like it!\
+So basically, this patch allows unlocks game option marked as "hide for online". For now there are only 3 such options: Map and impostor amount
+
+# How to install
+For now this patch has 3 versions: [static](https://github.com/Galster2010/GameSettingsUnlocker/tree/static), [runtime](https://github.com/Galster2010/GameSettingsUnlocker/tree/runtime) and [BepInEx](https://github.com/Galster2010/GameSettingsUnlocker/tree/BepInEx)
+
+- [Static](https://github.com/Galster2010/GameSettingsUnlocker/tree/static) (recommended)
+    Patches game's executable, which means you can even need to launch it only once
+- [Runtime](https://github.com/Galster2010/GameSettingsUnlocker/tree/runtime)
+    For people who scared about file editing.\
+    It patches memory at runtime which means you have to launch every time you want to use it.
+- [BepInEx](https://github.com/Galster2010/GameSettingsUnlocker/tree/BepInEx)
+    For people who scared about file editing, but also have [BepInEx](https://github.com/BepInEx/BepInEx/) installed
+    Just a [BepInEx](https://github.com/BepInEx/BepInEx/) plugin which doesn't conflict with any others (i hope)
+
+# This is a static version
+To get others, click on required version above
+
+# How to use
+1. Get [latest static release](https://github.com/Galster2010/GameSettingsUnlocker/releases/latest) from [releases](https://github.com/Galster2010/GameSettingsUnlocker/releases/)
+2. Open executable. It should say `Waiting for Among Us...`
+3. Follow instructions in console
+
+# How to disable
+1. Open executable again. It should ask you if you want to revert the patch
+2. Answer `y` (yes)
+
+# Credits
+- `cos8o` file mapping
---
+++ src/main.cpp
@@ -0,0 +1,162 @@
+#include "main.hpp"
+
+void logError(std::string const& msg, bool showerrorcode = false)
+{
+    std::string message("ERROR: " + msg);
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+    if (showerrorcode)
+    {
+        message += " Error code - " + std::to_string(GetLastError());
+    }
+    std::cout << message << std::endl;
+    SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY);
+    system("pause > nul");
+    exit(1);
+}
+
+void logInfo(std::string const& message, const WORD color = NULL)
+{
+    if (color != NULL)
+    {
+        SetConsoleTextAttribute(hConsole, color);
+    }
+    std::cout << message << std::endl;
+    if (color != NULL)
+    {
+        SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY);
+    }
+}
+
+HANDLE createMap(std::wstring& str)
+{
+    HANDLE file = CreateFileW(
+        str.c_str(),
+        GENERIC_READ | GENERIC_WRITE,
+        FILE_SHARE_READ | FILE_SHARE_WRITE,
+        NULL,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL
+    );
+    if (file == INVALID_HANDLE_VALUE) return NULL;
+
+    HANDLE map = CreateFileMappingA(file, NULL, PAGE_READWRITE, NULL, NULL, NULL);
+    CloseHandle(file);
+
+    return map;
+}
+
+inline uintptr_t getMapBase(HANDLE& map)
+{
+    return reinterpret_cast<uintptr_t>(MapViewOfFile(
+            map,
+            FILE_MAP_READ | FILE_MAP_WRITE,
+            NULL,
+            NULL,
+            0
+    ));
+}
+
+inline bool freeMap(uintptr_t base, HANDLE& map)
+{
+    return UnmapViewOfFile(reinterpret_cast<LPCVOID>(base)) && CloseHandle(map);
+}
+
+std::wstring getProcessPath()
+{
+
+    HWND hwnd = FindWindowA(NULL, "Among Us");
+    logInfo("Waiting for Among Us...", WHITE_TEXT);
+
+    while (!hwnd)
+    {
+        Sleep(500);
+        hwnd = FindWindowA(NULL, "Among Us");
+    }
+
+    DWORD pid;
+    GetWindowThreadProcessId(hwnd, &pid);
+    HANDLE process = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ | SYNCHRONIZE, NULL, pid);
+    if (process == INVALID_HANDLE_VALUE)
+    {
+        logError("Cannot get process info.", true);
+    }
+
+    constexpr uint32_t MAX_UNICODE_PATH = 32767u;
+    wchar_t buf[MAX_UNICODE_PATH];
+    DWORD len = GetModuleFileNameEx(process, NULL, buf, MAX_UNICODE_PATH);
+    if (!len)
+    {
+        logError("Cannot find process path.");
+    }
+    std::wstring path(buf, buf + len);
+    logInfo("Close the game to continue", WHITE_TEXT);
+    //delete[] buf;
+    WaitForSingleObject(process, INFINITE);
+    return path;
+}
+
+int main()
+{
+    SetConsoleTitleA("Among Us Game settings unlocker");
+    logInfo("Game settings unlocker patch by Galster\n", GREEN_TEXT);
+
+    std::wstring aupath = getProcessPath();
+    aupath = aupath.substr(0, aupath.find_last_of(L"\\")) + L"\\GameAssembly.dll"; // safety 100
+
+    std::wcout << std::endl << L"Trying to open \"" << aupath << "\"..." << std::endl;
+    HANDLE map = createMap(aupath);
+    if (!map)
+    {
+        if (GetLastError() == ERROR_FILE_NOT_FOUND)
+        {
+            logError("Cannot find GameAssembly.dll");
+        }
+        logError("Failed mapping GameAssembly.dll", true);
+    }
+
+    uintptr_t base = getMapBase(map);
+    if(!base) logError("Failed mapping GameAssembly.dll", true);
+
+    uintptr_t address = base + PATCH.offset;
+    std::vector<byte> buf(PATCH.off.size());
+    memcpy(buf.data(), reinterpret_cast<void*>(address), buf.size());
+    bool reversePatch = false;
+
+    if (buf == PATCH.on)
+    {
+        reversePatch = true;
+        logInfo("Looks like you already have this patch. Do you want to remove it? (y/n)", WHITE_TEXT);
+        std::string answer;
+        std::cin >> answer;
+        if (answer != "y" && answer != "yes")
+        {
+            logInfo("Then i have nothing to do, cleaning...");
+            freeMap(base, map);
+            logInfo("done", WHITE_TEXT);
+            system("pause");
+            return 0;
+        }
+    }
+    else if (buf != PATCH.off)
+    {
+        logError("Byte signature is incorrect. Are you on v2020.12.9s Steam?");
+    }
+
+    logInfo("Patching...");
+    memcpy((void*)address, (reversePatch ? PATCH.off : PATCH.on).data(), PATCH.on.size());
+
+    if (!FlushViewOfFile(reinterpret_cast<LPCVOID>(address), PATCH.on.size()))
+    {
+        logError("Patching failed", true);
+    }
+
+    if (!freeMap(base, map))
+    {
+        logError("Failed to load map out. Patch may be failed", true);
+    }
+
+    logInfo("\nSuccessfully patched!", GREEN_TEXT);
+
+    system("pause");
+}
